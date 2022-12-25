41b5363b09db91ed1cbdb549406bd465afcbceb4
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2022-12-25 11:57:18 +0200

    do subtitle text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,12 @@ import styles from "../style/Banner.module.scss";
 const Banner = () => {
     return (
         <div className={styles.bannerWrapper}>
-            <div className={styles.banner}>
-         <h2>The fastest brunch delivery in <span>Your city</span></h2>
-         </div>
+            <div className={styles.bannerTitle}>
+                <h2>The fastest brunch delivery in <span>Your city</span></h2>
+                <div className={styles.bannerSubtitle}>
+                    <h5>This is my Exploration for Fruit - Food Delivery Landing Page 🍕. <br /> How about you?</h5>
+                </div>
+            </div>
         </div>
     )
 }
