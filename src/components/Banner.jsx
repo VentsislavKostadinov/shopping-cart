f1c3c7338e46678ce66a963b244a6528adf004a3
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2022-12-25 14:02:48 +0200

    fix banner titles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,8 @@ import styles from "../style/Banner.module.scss";
 const Banner = () => {
     return (
         <div className={styles.bannerWrapper}>
-            <div className={styles.bannerTitle}>
-                <h2>The fastest brunch delivery in <span>Your city</span></h2>
-                <div className={styles.bannerSubtitle}>
-                    <h5>This is my Exploration for Fruit - Food Delivery Landing Page 🍕. <br /> How about you?</h5>
-                </div>
-            </div>
+            <h2>The fastest brunch delivery in <span>Your city</span></h2><br />
+            <h5>This is my Exploration for Fruit - Food Delivery Landing Page 🍕. <br /> How about you?</h5>
         </div>
     )
 }
